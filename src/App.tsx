20e504ffade8740765d7bb1e90844e95b698ece9
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-06-02 00:28:09 +0200

    trash UI v2 prototype

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,91 @@ function App() {
           <CodeEditor setCodeToRender={setCode}/>
         </div>
       </section>
+
       <section className='optionsContainer'>
 
+        <div className='inputsGroup'>
+          <button style={{padding:'0 0.5rem'}}>Add A New Row</button>
+        </div>
+
+        <div className='inputsGroup'>
+          <input placeholder='HTMLElement Id'/>
+          <input placeholder='Tweakable Property'/>
+          <select>
+            <option value="rules">-- Property Type --</option>
+            <option value="style">Style</option>
+            <option value="attribute">Attribute</option>
+            <option value="value">Value</option>
+            <option value="innerHTML">InnerHTML</option>
+          </select>
+          <select>
+            <option value="rules">-- Set Some Rules --</option>
+            <option value="free">Free</option>
+            <option value="runtime">At Runtime</option>
+            <option value="list">Within A List</option>
+          </select>
+          <input placeholder='List Of Values'/>
+        </div>
+
+        <div className='inputsGroup' style={{opacity:'0.8'}}>
+          <input placeholder='HTMLElement Id'/>
+          <input placeholder='Tweakable Property'/>
+          <select>
+            <option value="rules">-- Property Type --</option>
+            <option value="Style">Style</option>
+            <option value="elementAttribute">Element Attribute</option>
+            <option value="value">Value</option>
+            <option value="innerHTML">InnerHTML</option>
+          </select>
+          <select>
+            <option value="rules">-- Set Some Rules --</option>
+            <option value="free">Free</option>
+            <option value="runtime">At Runtime</option>
+            <option value="list">Within A List</option>
+          </select>
+          <input placeholder='List Of Values'/>
+        </div>
+
+        <div className='inputsGroup' style={{opacity:'0.6'}}>
+          <input placeholder='HTMLElement Id'/>
+          <input placeholder='Tweakable Property'/>
+          <select>
+            <option value="rules">-- Property Type --</option>
+            <option value="Style">Style</option>
+            <option value="elementAttribute">Element Attribute</option>
+            <option value="value">Value</option>
+            <option value="innerHTML">InnerHTML</option>
+          </select>
+          <select>
+            <option value="rules">-- Set Some Rules --</option>
+            <option value="free">Free</option>
+            <option value="runtime">At Runtime</option>
+            <option value="list">Within A List</option>
+          </select>
+          <input placeholder='List Of Values'/>
+        </div>
+
+        <div className='inputsGroup' style={{opacity:'0.4'}}>
+          <input placeholder='HTMLElement Id'/>
+          <input placeholder='Tweakable Property'/>
+          <select>
+            <option value="rules">-- Property Type --</option>
+            <option value="Style">Style</option>
+            <option value="elementAttribute">Element Attribute</option>
+            <option value="value">Value</option>
+            <option value="innerHTML">InnerHTML</option>
+          </select>
+          <select>
+            <option value="rules">-- Set Some Rules --</option>
+            <option value="free">Free</option>
+            <option value="runtime">At Runtime</option>
+            <option value="list">Within A List</option>
+          </select>
+          <input placeholder='List Of Values'/>
+        </div>
+
       </section>
+
     </>
   )
 }
